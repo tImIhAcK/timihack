0fb67a999abf3180ef9ff0168d3f7313ef53713a
Adeniran John <adeniranjohn2016@gmail.com>
2023-05-27 17:24:32 +0100

    my website

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import SocialLinks from "./components/SocialLinks";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+
+function App() {
+  return (
+    <div className="">
+      <NavBar />
+      <Home />
+      <About />
+      <Portfolio />
+      <SocialLinks />
+    </div>
+  );
+}
+
+export default App;
